c51ce49e20f8143ea16094fda6b32e1279d8da01
Jeroen <jeroenvkasteren@gmail.com>
2023-12-08 15:41:55 +0100

    Day 8

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,5 +1,5 @@
-import numpy as np
 import re
+import math
 
 with open('day8', 'r') as f:
     file = f.read()
@@ -8,24 +8,39 @@ instructions, places = file.split('\n\n')
 place = places.split('\n')[0]
 locations = {}
 current = []
+notes = []
 for place in places.split('\n'):
     key = place.split('=')[0].strip()
-    locations[key] = [re.sub("[^a-zA-Z]+", '', str) for str in
+    locations[key] = [re.sub("[^a-zA-Z1-9]+", '', str) for str in
                       place.split('=')[1].split(',')]
     if key[2] == 'A':
         current.append(key)
+        notes.append([key, 0, 0])  # Key, first Z, second Z
 
-res = 0
-i = 0
-while res < 100000:
-    # if all currents end with Z, break
-    if ''.join(current)[0::1] == 'ZZZ':
-        break
-    for x in current:
-    if instructions[i] == 'L':
-        current = locations[current][0]
-    else:
-        current = locations[current][1]
-    res += 1
-    i = (i + 1) % len(instructions)
-print(res)
+for j, current_j in enumerate(current):
+    res = 0
+    found_z = 0
+    i = 0
+    while res < 1e6:
+        new = ''
+        # print(current_j, res)
+        destination = ''.join(current_j)[2::3]
+        if destination.count('Z') == len(destination):
+            if found_z == 0:
+                notes[j][1] = res
+                found_z = 1
+            else:
+                notes[j][2] = res
+                break
+        if instructions[i] == 'L':
+            new_location = locations[current_j][0]
+        else:
+            new_location = locations[current_j][1]
+        res += 1
+        i = (i + 1) % len(instructions)
+        current_j = new_location
+
+diff = [notes[2] - notes[1] for notes in notes]
+print(notes)
+
+print(math.lcm(*diff))
