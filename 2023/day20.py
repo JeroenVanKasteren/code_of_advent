26b1431e19124a938d595425c6f9ea672d781e37
JeroenVanKasteren <jeroenvkasteren@gmail.com>
2023-12-21 14:31:32 +0100

    Getting back up track

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,68 +1,80 @@
-import numpy as np
-
 with open('day20', 'r') as f:
-    file = f.read()
+    file = f.read().split('\n')
 
-# file = """1
-# 2
-# -3
-# 3
-# -2
-# 0
-# 4"""
-numbers = np.array([int(number) for number in file.split('\n')])
-indices = np.arange(len(numbers))
+modules = {}
+for line in file:
+    source, destination = line.split(' -> ')
+    modules[source[1:]] = [source[0], destination.split(', ')]
 
-for i, number in enumerate(numbers):
-    start = indices[i]
-    end = (indices[i] + number) % (len(numbers) - 1)
-    # print(i, number, start, end)
-    if number > 0:
-        if start < end:
-            indices[(start < indices) & (indices <= end)] -= 1
-        elif end < start:
-            indices[(end <= indices) & (indices < start)] += 1
-    elif number < 0:
-        if end < start:
-            indices[(end <= indices) & (indices < start)] += 1
-        elif start < end:
-            indices[(start < indices) & (indices <= end)] -= 1
-    indices[i] = end
-    # print(numbers, indices, numbers[indices.argsort()])
-    # print('next')
+states = {}
+for name, (module, _) in modules.items():
+    if module == 'b':
+        states[name] = '-'
+    if module == '%':
+        states[name] = 'off'
+    if module == '&':
+        states[name] = {}
 
-print(numbers[indices.argsort()][(indices[numbers == 0][0] + 1000) % len(numbers)])
-print(numbers[indices.argsort()][(indices[numbers == 0][0] + 2000) % len(numbers)])
-print(numbers[indices.argsort()][(indices[numbers == 0][0] + 3000) % len(numbers)])
-print(numbers[indices.argsort()][(indices[numbers == 0][0] + 1000) % len(numbers)] +
-      numbers[indices.argsort()][(indices[numbers == 0][0] + 2000) % len(numbers)] +
-      numbers[indices.argsort()][(indices[numbers == 0][0] + 3000) % len(numbers)])
-# 7395
+for source, (_, destinations) in modules.items():
+    for destination in destinations:
+        if destination in modules:
+            if modules[destination][0] == '&':
+                states[destination][source] = 'low'
 
-# Part 2
-numbers = np.array([int(number) for number in file.split('\n')], dtype="int64")
-indices = np.arange(len(numbers))
+pushes = 0
+goal = 100000
+invert = {'on': 'off', 'off': 'on'}
+invert_pulse = {'high': 'low', 'low': 'high'}
+memory = {}
+pulse_count = {'high': 0, 'low': 0}
+cycle_found = False
+start_state_hash = hash(str(states))
 
-DECRYPTION_KEY = 811589153
-MIXES = 10
-numbers *= DECRYPTION_KEY
 
-for _ in range(MIXES):
-    for i, number in enumerate(numbers):
-        start = indices[i]
-        end = (indices[i] + number) % (len(numbers) - 1)
-        if number > 0:
-            if start < end:
-                indices[(start < indices) & (indices <= end)] -= 1
-            elif end < start:
-                indices[(end <= indices) & (indices < start)] += 1
-        elif number < 0:
-            if end < start:
-                indices[(end <= indices) & (indices < start)] += 1
-            elif start < end:
-                indices[(start < indices) & (indices <= end)] -= 1
-        indices[i] = end
+def send(queue, pulse_count, pulse, sent_to):
+    for destination in sent_to:
+        # print(pulse, 'to', destination)
+        pulse_count[pulse] += 1
+        if destination == 'rx' and pulse == 'low':
+            print(pushes)
+            exit(0)
+        if destination in modules:
+            queue.append([destination, pulse, receiver])
+    return queue, pulse_count
 
-print(numbers[indices.argsort()][(indices[numbers == 0][0] + 1000) % len(numbers)] +
-      numbers[indices.argsort()][(indices[numbers == 0][0] + 2000) % len(numbers)] +
-      numbers[indices.argsort()][(indices[numbers == 0][0] + 3000) % len(numbers)])
+
+def predict(goal, pushes, pulse_count):
+    res = 1
+    for pulse, count in pulse_count.items():
+        res *= count * goal / pushes
+    print(res)
+
+while pushes < goal:
+    pushes += 1
+    queue = [('roadcaster', 'low', 'button')]
+    pulse_count['low'] += 1
+    while len(queue) > 0:
+        receiver, pulse, sender = queue.pop(0)
+        state = states[receiver]
+        module, sent_to = modules[receiver]
+
+        if module == 'b':
+            queue, pulse_count = send(queue, pulse_count, pulse, sent_to)
+        if module == '%' and pulse == 'low':
+            states[receiver] = invert[state]
+            pulse = 'high' if states[receiver] == 'on' else 'low'
+            queue, pulse_count = send(queue, pulse_count, pulse, sent_to)
+        if module == '&':
+            states[receiver][sender] = pulse
+            if len(set(states[receiver].values())) == 1 and pulse == 'high':
+                pulse = invert_pulse[pulse]
+                queue, pulse_count = send(queue, pulse_count, 'low', sent_to)
+            else:
+                queue, pulse_count = send(queue, pulse_count, 'high', sent_to)
+    hash_of_state = hash(str(states))
+    if hash_of_state == start_state_hash:
+        print('cycle found', pushes)
+        predict(goal, pushes, pulse_count)
+        print(pulse_count, pulse_count['low'] * pulse_count['high'])
+        break
+print(pulse_count, pulse_count['low']*pulse_count['high'])
--- 2023/day21.py
+++ 2023/day21.py
@@ -1,216 +1,74 @@
-import scipy
-
 with open('day21', 'r') as f:
-    file = f.read()
+    map = f.read().split('\n')
 
-report = [line.split(': ') for line in file.split('\n')]
-monkeys = {}
-for line in report:
-    if line[1].isnumeric():
-        monkeys[line[0]] = int(line[1])
-    else:
-        monkeys[line[0]] = line[1].split(' ')
+dirs = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (-1, 0)}
+current = set()  # (r, c)
+unvisited = set()
 
-
-def get_number(name):
-    global monkeys
-
-    if isinstance(monkeys[name], int):
-        return monkeys[name]
-
-    a = get_number(monkeys[name][0])
-    b = get_number(monkeys[name][2])
-    if monkeys[name][1] == '+':
-        monkeys[name] = a + b
-        return a + b
-    elif monkeys[name][1] == '-':
-        monkeys[name] = a - b
-        return a - b
-    elif monkeys[name][1] == '*':
-        monkeys[name] = a * b
-        return a * b
-    elif monkeys[name][1] == '/':
-        monkeys[name] = a / b
-        return a / b
+for r, row in enumerate(map):
+    for c, char in enumerate(row):
+        if char == '.':
+            unvisited.add((r, c))
+        elif char == 'S':
+            current.add((r, c))
 
 
-print(get_number('root'))
-
-print(monkeys)
-print(monkeys['root'])
-
-# ###############################################################################################
-# ########## part 2, solution with string formula (eval(string)) and scipy optimize #############
-# ###############################################################################################
-
-report = [line.split(': ') for line in file.split('\n')]
-monkeys = {}
-for line in report:
-    if line[1].isnumeric():
-        monkeys[line[0]] = float(line[1])
-    else:
-        monkeys[line[0]] = line[1].split(' ')
+# def pos_visits(steps, goal):
+#     if steps % 2 == goal % 2:
+#         times = goal // 2
+#     else:
+#         times = goal // 2 - 1
+#     return times - steps // 2 + 1
 
 
-def normal_arithmatic(name, a, b):
-    global monkeys
-
-    if monkeys[name][1] == '+':
-        monkeys[name] = a + b
-        return a + b
-    elif monkeys[name][1] == '-':
-        monkeys[name] = a - b
-        return a - b
-    elif monkeys[name][1] == '*':
-        monkeys[name] = a * b
-        return a * b
-    else:  # monkeys[name][1] == '/':
-        monkeys[name] = a / b
-        return a / b
+def visits_at_end(steps, goal):
+    return 1 if steps % 2 == goal % 2 else 0
 
 
-def get_number_string(name):
-    global monkeys
-
-    if name == 'humn':
-        return 'x'
-    if isinstance(monkeys[name], float):
-        return monkeys[name]
-
-    a = get_number_string(monkeys[name][0])
-    b = get_number_string(monkeys[name][2])
-    if isinstance(a, float) and isinstance(b, float):
-        return normal_arithmatic(name, a, b)
-    return '('+str(a) + monkeys[name][1] + str(b)+')'
-
-
-print(get_number_string(monkeys['root'][0]))
-print(get_number_string(monkeys['root'][2]))
-
-y = get_number_string(monkeys['root'][2])
-formula = get_number_string(monkeys['root'][0]) + '-' + str(y)
+goal = 64
+steps = 0
+res = visits_at_end(steps, goal)
+while steps < goal and len(current) > 0:
+    steps += 1
+    next_places = set()
+    for node in current:
+        for d in dirs.values():
+            r, c = node[0] + d[0], node[1] + d[1]
+            if (r, c) in unvisited:
+                next_places.add((r, c))
+                unvisited.remove((r, c))
+    res += len(next_places) * visits_at_end(steps, goal)
+    current = next_places
+print(res)
 
 
-def f(x):
-    global formula
+# Part 2
+current = set()
+visited = set()
 
-    return abs(eval(formula))
-
-
-max_x = scipy.optimize.fmin(f, 10000000)
-print('{:.20f}'.format(max_x[0]))
-
-# ###############################################################################################
-# ################################### part 2,puzzle solution ####################################
-# ###############################################################################################
-
-# file = """root: pppw + sjmn
-# dbpl: 5
-# cczh: sllz + lgvd
-# zczc: 2
-# ptdq: humn - dvpt
-# dvpt: 3
-# lfqf: 4
-# humn: 5
-# ljgn: 2
-# sjmn: drzm * dbpl
-# sllz: 4
-# pppw: cczh / lfqf
-# lgvd: ljgn * ptdq
-# drzm: hmdt - zczc
-# hmdt: 32"""
+for r, row in enumerate(map):
+    for c, char in enumerate(row):
+        if char == 'S':
+            current.add((r, c))
+            visited.add((r, c))
 
 
-report = [line.split(': ') for line in file.split('\n')]
-monkeys = {}
-for line in report:
-    if line[1].isnumeric():
-        monkeys[line[0]] = float(line[1])
-    else:
-        monkeys[line[0]] = line[1].split(' ')
+def valid_position(r, c):
+    return map[r % len(map)][c % len(map[0])] != '#'
 
 
-def normal_arithmatic(name, a, b):
-    global monkeys
-
-    if monkeys[name][1] == '+':
-        monkeys[name] = a + b
-        return a + b
-    elif monkeys[name][1] == '-':
-        monkeys[name] = a - b
-        return a - b
-    elif monkeys[name][1] == '*':
-        monkeys[name] = a * b
-        return a * b
-    else:  # monkeys[name][1] == '/':
-        monkeys[name] = a / b
-        return a / b
-
-
-def get_number2(name):
-    global monkeys
-
-    if name == 'humn':
-        return 'x'
-    elif isinstance(monkeys[name], float):
-        return monkeys[name]
-
-    a = get_number2(monkeys[name][0])
-    b = get_number2(monkeys[name][2])
-    if isinstance(a, float) and isinstance(b, float):
-        return normal_arithmatic(name, a, b)
-    elif isinstance(a, float):
-        monkeys[monkeys[name][0]] = a
-    elif isinstance(b, float):
-        monkeys[monkeys[name][2]] = b
-    return 'x'
-
-
-def reverse_arithmatic(name, a, y, left):
-    global monkeys
-
-    if monkeys[name][1] == '+':
-        monkeys[name] = y - a
-        return y - a
-    elif monkeys[name][1] == '-' and left:
-        monkeys[name] = a - y
-        return a - y
-    elif monkeys[name][1] == '-' and not left:
-        monkeys[name] = y + a
-        return y + a
-    elif monkeys[name][1] == '/' and left:
-        monkeys[name] = a / y
-        return a / y
-    elif monkeys[name][1] == '/' and not left:
-        monkeys[name] = a * y
-        return a * y
-    elif monkeys[name][1] == '*':
-        monkeys[name] = y / a
-        return y / a
-
-
-def solve_x(name, y):
-    global monkeys
-
-    a = monkeys[monkeys[name][0]]
-    b = monkeys[monkeys[name][2]]
-
-    if monkeys[name][0] == 'humn':
-        print(reverse_arithmatic(name, b, y, left=False))
-    elif monkeys[name][2] == 'humn':
-        print(reverse_arithmatic(name, a, y, left=True))
-    elif isinstance(monkeys[name], float):
-        return monkeys[name]
-    elif isinstance(a, float):
-        solve_x(monkeys[name][2], reverse_arithmatic(name, a, y, left=True))
-    elif isinstance(b, float):
-        solve_x(monkeys[name][0], reverse_arithmatic(name, b, y, left=False))
-
-
-get_number2(monkeys['root'][0])
-get_number2(monkeys['root'][2])
-
-if isinstance(monkeys[monkeys['root'][0]], float):
-    solve_x(monkeys['root'][2], monkeys[monkeys['root'][0]])
-else:
-    solve_x(monkeys['root'][0], monkeys[monkeys['root'][2]])
+goal = 5000
+steps = 0
+res = visits_at_end(steps, goal)
+while steps < goal and len(current) > 0:
+    steps += 1
+    next_places = set()
+    for node in current:
+        for d in dirs.values():
+            r, c = node[0] + d[0], node[1] + d[1]
+            if valid_position(r, c) and (r, c) not in visited:
+                next_places.add((r, c))
+                visited.add((r, c))
+    res += len(next_places) * visits_at_end(steps, goal)
+    current = next_places
+print(res)
